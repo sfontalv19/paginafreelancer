4906ec44d0821758c54a928e26537a98d687a991
sfontalv19 <serme1009@gmail.com>
2024-10-08 15:15:15 -0500

    actualizando mi portafolio

---
+++ pagina/script.js
@@ -0,0 +1,31 @@
+function dirigir(){
+    
+    var formulario = document.getElementById("formulario");
+
+    formulario.scrollIntoView();
+
+
+}
+document.addEventListener("DOMContentLoaded", dirigir);
+
+
+function dirigirQuien(){
+    const sobreMI = document.getElementById('quien');
+
+    sobreMI.scrollIntoView();
+
+}
+document.addEventListener('DOMContentLoaded', dirigirQuien)
+
+
+// darle estilo a la ventana de mi titulo
+let previousTitle = document.title; 
+
+window.addEventListener('blur', () => {
+    previousTitle = document.title; 
+    document.title = "No te vayas, vuelve 😥"; 
+});
+
+window.addEventListener('focus', () => {
+    document.title = previousTitle; 
+});
